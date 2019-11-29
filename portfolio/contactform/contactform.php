5b156dbb9bfa900db0109c33cb21cb49372cb7c8
ra-duga58 <ra-duga58@mail.ru>
2019-11-29 12:14:21 +0300

    mail

--- portfolio/contactform/contactform.php
+++ portfolio/contactform/contactform.php
@@ -1,10 +1,4 @@
 <?php
-  /**
-  * Requires the PHP Mail Form library
-  * The PHP Mail Form library is available only in the pro version of the template
-  * The library should be uploaded to: lib/php-mail-form/php-mail-form.php
-  * For more info and help: https://templatemag.com/php-mail-form/
-  */
 
   if( file_exists($php_mail_form_library = '../lib/php-mail-form/php-mail-form.php' )) {
     include( $php_mail_form_library );
@@ -15,8 +9,8 @@
   $contactform = new PHP_Mail_Form;
   $contactform->ajax = true;
 
-  // Replace with your real receiving email address
-  $contactform->to = 'contact@example.com';
+
+  $contactform->to = 'kockocja@gmail.com';
   $contactform->from_name = $_POST['name'];
   $contactform->from_email = $_POST['email'];
   $contactform->subject = $_POST['subject'];
